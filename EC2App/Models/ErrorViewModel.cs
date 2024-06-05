f2d7ea0ee0fc36dcef97a7dbb8580c820cbaa41b
Praveen Balakrishnan <pravi.krishna@gmail.com>
2024-06-05 15:08:21 +0530

    Initial Check in
    Initial Check in

---
+++ EC2App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EC2App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
